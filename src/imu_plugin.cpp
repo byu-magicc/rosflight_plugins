1e5f35ccfd61f7cabd66bf69f5cdaaede391c0dd
Trey Henrichsen <treyhenrichsen@gmail.com>
2018-07-19 15:53:24 -0600

    imu_plugin.cpp working with gazebo 9

--- src/imu_plugin.cpp
+++ src/imu_plugin.cpp
@@ -57,7 +57,11 @@ void ImuPlugin::Load(gazebo::physics::ModelPtr _model, sdf::ElementPtr _sdf)
   namespace_.clear();
 
 
+#if GAZEBO_MAJOR_VERSION >=8
+  gravity_ = world_->GetPhysicsEngine()->Gravity();
+#else
   gravity_ = world_->GetPhysicsEngine()->GetGravity();
+#endif
 
   //
   // Get elements from the robot urdf/sdf file
@@ -78,7 +82,11 @@ void ImuPlugin::Load(gazebo::physics::ModelPtr _model, sdf::ElementPtr _sdf)
     gzthrow("[imu_plugin] Couldn't find specified link \"" << link_name_ << "\".");
 
   // Get mass
+#if GAZEBO_MAJOR_VERSION >=8
+  mass_ = link_->GetInertial()->Mass();
+#else
   mass_ = link_->GetInertial()->GetMass();
+#endif
 
   //
   // ROS Node Setup
@@ -115,6 +123,32 @@ void ImuPlugin::Load(gazebo::physics::ModelPtr _model, sdf::ElementPtr _sdf)
   normal_distribution_ = std::normal_distribution<double>(0.0, 1.0);
   uniform_distribution_ = std::uniform_real_distribution<double>(-1.0, 1.0);
 
+#if GAZEBO_MAJOR_VERSION >= 8
+  // Turn off noise and bias if noise_on_ disabled
+  if (!noise_on_)
+  {
+    gyro_stdev_ = 0;
+    gyro_bias_range_ = 0;
+    gyro_bias_walk_stdev_ = 0;
+    gyro_bias_.X(0);
+    gyro_bias_.Y(0);
+    gyro_bias_.Z(0);
+    acc_stdev_ = 0;
+    acc_bias_range_ = 0;
+    acc_bias_walk_stdev_ = 0;
+    acc_bias_.X(0);
+    acc_bias_.Y(0);
+    acc_bias_.Z(0);
+  }
+
+  // Initialize the Bias
+  gyro_bias_.X(gyro_bias_range_*uniform_distribution_(random_generator_));
+  gyro_bias_.Y(gyro_bias_range_*uniform_distribution_(random_generator_));
+  gyro_bias_.Z(gyro_bias_range_*uniform_distribution_(random_generator_));
+  acc_bias_.X(acc_bias_range_*uniform_distribution_(random_generator_));
+  acc_bias_.Y(acc_bias_range_*uniform_distribution_(random_generator_));
+  acc_bias_.Z(acc_bias_range_*uniform_distribution_(random_generator_));
+#else
   // Turn off noise and bias if noise_on_ disabled
   if (!noise_on_)
   {
@@ -139,6 +173,7 @@ void ImuPlugin::Load(gazebo::physics::ModelPtr _model, sdf::ElementPtr _sdf)
   acc_bias_.x = acc_bias_range_*uniform_distribution_(random_generator_);
   acc_bias_.y = acc_bias_range_*uniform_distribution_(random_generator_);
   acc_bias_.z = acc_bias_range_*uniform_distribution_(random_generator_);
+#endif
  
   // Set up static members of IMU message
   imu_message_.header.frame_id = link_name_;
@@ -174,13 +209,29 @@ void ImuPlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
   if ((current_time - last_time_).Double() >= sample_time_)
   {
 #if GAZEBO_MAJOR_VERSION >= 8
-    ignition::math::Quaterniond q_I_NWU = link_->GetWorldPose().rot;
-    ignition::math::Vector3d omega_B_NWU = link_->GetRelativeAngularVel();
-    ignition::math::Vector3d uvw_B_NWU = link_->GetRelativeLinearVel();
+    ignition::math::Quaterniond q_I_NWU = link_->WorldPose().Rot();
+    ignition::math::Vector3d omega_B_NWU = link_->RelativeAngularVel();
+    ignition::math::Vector3d uvw_B_NWU = link_->RelativeLinearVel();
 
     // y_acc = F/m - R*g
-    ignition::math::Vector3d y_acc = link_->GetRelativeForce()/mass_ - q_I_NWU.RotateVectorReverse(gravity_);
-    ignition::math::Vector3d y_gyro = link_->GetRelativeAngularVel();
+    ignition::math::Vector3d y_acc = link_->RelativeForce()/mass_ - q_I_NWU.RotateVectorReverse(gravity_);
+    ignition::math::Vector3d y_gyro = link_->RelativeAngularVel();
+
+    // Apply normal noise
+    y_acc.X(y_acc.X() + acc_stdev_*normal_distribution_(random_generator_));
+    y_acc.Y(y_acc.Y() + acc_stdev_*normal_distribution_(random_generator_));
+    y_acc.Z(y_acc.Z() + acc_stdev_*normal_distribution_(random_generator_));
+    y_gyro.X(y_gyro.X() + gyro_stdev_*normal_distribution_(random_generator_));
+    y_gyro.Y(y_gyro.Y() + gyro_stdev_*normal_distribution_(random_generator_));
+    y_gyro.Z(y_gyro.Z() + gyro_stdev_*normal_distribution_(random_generator_));
+
+    // Perform Random Walk for biases
+    acc_bias_.X(acc_bias_.X() + acc_bias_walk_stdev_*normal_distribution_(random_generator_));
+    acc_bias_.Y(acc_bias_.Y() + acc_bias_walk_stdev_*normal_distribution_(random_generator_));
+    acc_bias_.Z(acc_bias_.Z() + acc_bias_walk_stdev_*normal_distribution_(random_generator_));
+    gyro_bias_.X(gyro_bias_.X() + gyro_bias_walk_stdev_*normal_distribution_(random_generator_));
+    gyro_bias_.Y(gyro_bias_.Y() + gyro_bias_walk_stdev_*normal_distribution_(random_generator_));
+    gyro_bias_.Z(gyro_bias_.Z() + gyro_bias_walk_stdev_*normal_distribution_(random_generator_));
 #else
     gazebo::math::Quaternion q_I_NWU = link_->GetWorldPose().rot;
     gazebo::math::Vector3 omega_B_NWU = link_->GetRelativeAngularVel();
@@ -189,7 +240,6 @@ void ImuPlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
     // y_acc = F/m - R*g
     gazebo::math::Vector3 y_acc = link_->GetRelativeForce()/mass_ - q_I_NWU.RotateVectorReverse(gravity_);
     gazebo::math::Vector3 y_gyro = link_->GetRelativeAngularVel();
-#endif
 
     // Apply normal noise
     y_acc.x += acc_stdev_*normal_distribution_(random_generator_);
@@ -206,14 +256,25 @@ void ImuPlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
     gyro_bias_.x += gyro_bias_walk_stdev_*normal_distribution_(random_generator_);
     gyro_bias_.y += gyro_bias_walk_stdev_*normal_distribution_(random_generator_);
     gyro_bias_.z += gyro_bias_walk_stdev_*normal_distribution_(random_generator_);
+#endif
+
 
     // Add constant Bias to measurement
+#if GAZEBO_MAJOR_VERSION >= 8
+    y_acc.X(y_acc.X() + acc_bias_.X());
+    y_acc.Y(y_acc.Y() + acc_bias_.Y());
+    y_acc.Z(y_acc.Z() + acc_bias_.Z());
+    y_gyro.X(y_gyro.X() + gyro_bias_.X());
+    y_gyro.Y(y_gyro.Y() + gyro_bias_.Y());
+    y_gyro.Z(y_gyro.Z() + gyro_bias_.Z());
+#else
     y_acc.x += acc_bias_.x;
     y_acc.y += acc_bias_.y;
     y_acc.z += acc_bias_.z;
     y_gyro.x += gyro_bias_.x;
     y_gyro.y += gyro_bias_.y;
     y_gyro.z += gyro_bias_.z;
+#endif
 
     // Set Time stamp
     imu_message_.header.stamp.sec = current_time.sec;
@@ -224,7 +285,21 @@ void ImuPlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
     imu_message_.orientation.x = 0;
     imu_message_.orientation.y = 0;
     imu_message_.orientation.z = 0;
-    imu_message_.orientation.w = q_I_NWU.w;
+
+#if GAZEBO_MAJOR_VERSION >= 8
+    imu_message_.orientation.w = q_I_NWU.W();
+    imu_message_.orientation.x = q_I_NWU.X();
+    imu_message_.orientation.y = -q_I_NWU.Y();
+    imu_message_.orientation.z = -q_I_NWU.Z();
+
+    imu_message_.linear_acceleration.x = y_acc.X();
+    imu_message_.linear_acceleration.y = -y_acc.Y();
+    imu_message_.linear_acceleration.z = -y_acc.Z();
+    imu_message_.angular_velocity.x = y_gyro.X();
+    imu_message_.angular_velocity.y = -y_gyro.Y();
+    imu_message_.angular_velocity.z = -y_gyro.Z();
+#else
+    imu_message_.orientation.w = q_I_NWU.W;
     imu_message_.orientation.x = q_I_NWU.x;
     imu_message_.orientation.y = -q_I_NWU.y;
     imu_message_.orientation.z = -q_I_NWU.z;
@@ -235,6 +310,7 @@ void ImuPlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
     imu_message_.angular_velocity.x = y_gyro.x;
     imu_message_.angular_velocity.y = -y_gyro.y;
     imu_message_.angular_velocity.z = -y_gyro.z;
+#endif
 
     imu_pub_.publish(imu_message_);
 
@@ -245,6 +321,19 @@ void ImuPlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
     acc_bias_msg.header = imu_message_.header;
     gyro_bias_msg.header = imu_message_.header;
 
+#if GAZEBO_MAJOR_VERSION >= 8
+    // Convert Bias to NED
+    acc_bias_msg.vector.x = acc_bias_.X();
+    acc_bias_msg.vector.y = -acc_bias_.Y();
+    acc_bias_msg.vector.z = -acc_bias_.Z();
+    acc_bias_pub_.publish(acc_bias_msg);
+
+    // Convert Bias to NED
+    gyro_bias_msg.vector.x = gyro_bias_.X();
+    gyro_bias_msg.vector.y = -gyro_bias_.Y();
+    gyro_bias_msg.vector.z = -gyro_bias_.Z();
+    gyro_bias_pub_.publish(gyro_bias_msg);
+#else
     // Convert Bias to NED
     acc_bias_msg.vector.x = acc_bias_.x;
     acc_bias_msg.vector.y = -acc_bias_.y;
@@ -256,6 +345,7 @@ void ImuPlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
     gyro_bias_msg.vector.y = -gyro_bias_.y;
     gyro_bias_msg.vector.z = -gyro_bias_.z;
     gyro_bias_pub_.publish(gyro_bias_msg);
+#endif
   }
 }
 
