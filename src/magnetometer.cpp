8aa898624de526e77ca221a52c25cf7d0bf9c6a3
Trey Henrichsen <treyhenrichsen@gmail.com>
2018-07-19 16:06:34 -0600

    Fix magnetometer

--- src/magnetometer.cpp
+++ src/magnetometer.cpp
@@ -100,6 +100,18 @@ void MagnetometerPlugin::Load(gazebo::physics::ModelPtr _model, sdf::ElementPtr 
   normal_dist_ = std::normal_distribution<double>(0.0, 1.0);
   uniform_dist_ = std::uniform_real_distribution<double>(-bias_range_, bias_range_);
 
+#if GAZEBO_MAJOR_VERSION >= 8
+  // Create a bias offset
+  bias_vector_.X(uniform_dist_(random_gen_));
+  bias_vector_.Y(uniform_dist_(random_gen_));
+  bias_vector_.Z(uniform_dist_(random_gen_));
+
+  // Figure out inertial magnetic field
+  // Gazebo coordinates is NWU and Earth's magnetic field is defined in NED, hence the negative signs
+  inertial_magnetic_field_.Z(sin(-inclination_));
+  inertial_magnetic_field_.X(cos(-inclination_)*cos(-declination_));
+  inertial_magnetic_field_.Y(cos(-inclination_)*sin(-declination_));
+#else
   // Create a bias offset
   bias_vector_.x = uniform_dist_(random_gen_);
   bias_vector_.y = uniform_dist_(random_gen_);
@@ -110,14 +122,21 @@ void MagnetometerPlugin::Load(gazebo::physics::ModelPtr _model, sdf::ElementPtr 
   inertial_magnetic_field_.z = sin(-inclination_);
   inertial_magnetic_field_.x = cos(-inclination_)*cos(-declination_);
   inertial_magnetic_field_.y = cos(-inclination_)*sin(-declination_);
+#endif
 
   // turn off noise and bias of noise_on is disabled
   if (!noise_on_)
   {
     noise_sigma_ = 0;
+#if GAZEBO_MAJOR_VERSION >= 8
+    bias_vector_.X(0);
+    bias_vector_.Y(0);
+    bias_vector_.Z(0);
+#else
     bias_vector_.x = 0;
     bias_vector_.y = 0;
     bias_vector_.z = 0;
+#endif
   }
 
   // Fill in static members of message
@@ -141,25 +160,23 @@ void MagnetometerPlugin::OnUpdate(const gazebo::common::UpdateInfo& _info)
   if ((current_time - last_time_).Double() >= sample_time_) {
 
 #if GAZEBO_MAJOR_VERSION >= 8
-    ignition::math::Pose3d I_to_B = link_->GetWorldPose();
+    ignition::math::Pose3d I_to_B = link_->WorldPose();
 
     ignition::math::Vector3d noise;
-    //TODO fix this for gazebo 8+
-    noise.x = noise_sigma_*normal_dist_(random_gen_);
-    noise.y = noise_sigma_*normal_dist_(random_gen_);
-    noise.z = noise_sigma_*normal_dist_(random_gen_);
+    noise.X(noise_sigma_*normal_dist_(random_gen_));
+    noise.Y(noise_sigma_*normal_dist_(random_gen_));
+    noise.Z(noise_sigma_*normal_dist_(random_gen_));
 
     // combine parts to create a measurement
-    ignition::math::Vector3d measurement = I_to_B.rot.RotateVectorReverse(inertial_magnetic_field_) + noise + bias_vector_;
+    ignition::math::Vector3d measurement = I_to_B.Rot().RotateVectorReverse(inertial_magnetic_field_) + noise + bias_vector_;
 
     // normalize measurement
     ignition::math::Vector3d normalized = measurement.Normalize();
 
     mag_msg_.header.stamp.fromSec(world_->SimTime().Double());
-    //TODO fix this for Gazebo 8+
-    mag_msg_.magnetic_field.x =  normalized.x;
-    mag_msg_.magnetic_field.y = -normalized.y; // convert to NED for publishing
-    mag_msg_.magnetic_field.z = -normalized.z;
+    mag_msg_.magnetic_field.x =  normalized.X();
+    mag_msg_.magnetic_field.y = -normalized.Y(); // convert to NED for publishing
+    mag_msg_.magnetic_field.z = -normalized.Z();
 #else
     gazebo::math::Pose I_to_B = link_->GetWorldPose();
 
